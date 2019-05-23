ba5b6fa23b749ee59f973216811fb8b0ec6ebed8
xmmt <mkmax472@gmail.com>
2019-05-24 01:18:04 +0300

    fix fractionset implementation

--- Lab1/src/main/java/Fraction.java
+++ Lab1/src/main/java/Fraction.java
@@ -1,9 +1,22 @@
-public class Fraction {
+public class Fraction implements Comparable<Fraction> {
     private long m, n;
 
     Fraction(long m, long n) {
+        long g = gcd(m, n);
+        this.m = m / g;
+        this.n = n / g;
+    }
+    Fraction(long m) {
         this.m = m;
-        this.n = n;
+        this.n = 1;
+    }
+
+    private long gcd(long a, long b) {
+        while (b != 0) {
+            a %= b;
+            a ^= b ^= a ^= b;
+        }
+        return a;
     }
 
     public long getNumerator() { return m; }
@@ -14,11 +27,15 @@ public class Fraction {
 
     public void setDenominator(long n) { this.n = n; }
 
-    public boolean isLessThan(Fraction f) {
-        return m * f.getNumerator() < n * f.getDenominator();
-    }
-
-    public boolean isEqual(Fraction f) {
-        return m == f.getNumerator() && n == f.getDenominator();
+    public int compareTo(Fraction o) {
+        long l = n * o.n / gcd(n, o.n);
+        long a = m * (l / o.n);
+        long b = o.m / (l / n);
+        if (a > b)
+            return 1;
+        else if (a < b)
+            return -1;
+        else
+            return 0;
     }
 }
