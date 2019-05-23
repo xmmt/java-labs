898f58349be8a4d1065eb75cf48d240a80dc509c
xmmt <mkmax472@gmail.com>
2019-05-24 00:46:10 +0300

    Fraction implementation

--- Lab1/src/main/java/Fraction.java
+++ Lab1/src/main/java/Fraction.java
@@ -1,3 +1,20 @@
 public class Fraction {
-    private double m, n;
+    private long m, n;
+
+    Fraction(long m, long n) {
+        this.m = m;
+        this.n = n;
+    }
+
+    public long getNumerator() { return m; }
+
+    public long getDenominator() { return n; }
+
+    public void setNumerator(long m) { this.m = m; }
+
+    public void setDenominator(long n) { this.n = n; }
+
+    public boolean isLessThan(Fraction f) {
+        return m * f.getNumerator() < n * f.getDenominator();
+    }
 }
